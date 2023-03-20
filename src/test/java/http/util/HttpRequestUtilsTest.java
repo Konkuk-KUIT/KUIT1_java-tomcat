dea7a63fc63d0fb676bc113aeb19b0fb6eb04485
JungWoo <rlawjddn103@naver.com>
2023-03-21 00:30:04 +0900

    fix(HttpRequestUtils): 문자열 처리 도중 쿼리문 형태가 아니어서 오류가 날 경우 빈 Map 반환하게 변경

--- src/test/java/http/util/HttpRequestUtilsTest.java
+++ src/test/java/http/util/HttpRequestUtilsTest.java
@@ -9,14 +9,19 @@ class HttpRequestUtilsTest {
 
     @Test
     void parseQuery() {
-        Map<String, String> queryParameter = HttpRequestUtils.getQueryParameter("userId=1");
+        Map<String, String> queryParameter = HttpRequestUtils.parseQueryParameter("userId=1");
         assertEquals("1",queryParameter.get("userId"));
     }
 
     @Test
     void parseQueryMore() {
-        Map<String, String> queryParameter = HttpRequestUtils.getQueryParameter("userId=1&password=1");
+        Map<String, String> queryParameter = HttpRequestUtils.parseQueryParameter("userId=1&password=1");
         assertEquals("1",queryParameter.get("userId"));
         assertEquals("1",queryParameter.get("password"));
+    }
+
+    @Test
+    void parseQueryZero() {
+        Map<String, String> queryParameter = HttpRequestUtils.parseQueryParameter("");
     }
 }
