b72132894d0f1a4d5964c5f7ca1ab4020bf990eb
JungWoo <rlawjddn103@naver.com>
2023-04-01 11:17:02 +0900

    refactor: 도움되는 주석 추가

--- src/main/java/http/util/IOUtils.java
+++ src/main/java/http/util/IOUtils.java
@@ -4,6 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class IOUtils {
+    /**
+     *
+     * @param br
+     * socket으로부터 가져온 InputStream
+     *
+     * @param contentLength
+     * 헤더의 Content-Length의 값이 들어와야한다.
+     *
+     */
     public static String readData(BufferedReader br, int contentLength) throws IOException {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
