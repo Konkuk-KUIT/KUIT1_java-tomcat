94caea54e185d033f3dff859376e29c64059f3a3
jiyun <kelly574@naver.com>
2023-04-02 20:14:46 +0900

    feat: Complete step1 - requirement1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,11 +2,15 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RequestHandler implements Runnable{
     Socket connection;
+    private static final String ROOT = "./webapp";
+    private static final String MAIN_URL = "/index.html";
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
 
     public RequestHandler(Socket connection) {
@@ -20,13 +24,41 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
+            byte[] body = new byte[0];
+            body = readRequest(br, dos);
+
             response200Header(dos, body.length);
             responseBody(dos, body);
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
         }
+    }
+
+    private byte[] readRequest(BufferedReader br, DataOutputStream dos) throws IOException {
+
+        String startLine = br.readLine();
+        String[] startLines = startLine.split(" ");
+        String method = startLines[0];
+        String requestUrl = startLines[1];
+        String HTTP_version = startLines[2];
+
+        log.log(Level.INFO, "<" + requestUrl + ">");
+        byte[] body = new byte[0];
+
+
+        /* ****************************
+                    요구사항 1
+         ****************************** */
+        // 아무런 입력이 없다면 MAIN_URL(==index.html)로
+        if(requestUrl.equals("/")){
+            requestUrl = MAIN_URL;
+        }
+        if(method.equals("GET") && requestUrl.contains(".html")) {
+            body = Files.readAllBytes(Paths.get(ROOT + requestUrl));
+        }
+
+        return body;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
