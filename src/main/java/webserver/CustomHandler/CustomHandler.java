7653244e71a19332bc71534da8aa5a0312e6b981
hhj <gudwn357@gmail.com>
2023-04-02 18:54:16 +0900

    feat (CustomHandler) : add parameter

--- src/main/java/webserver/CustomHandler/CustomHandler.java
+++ src/main/java/webserver/CustomHandler/CustomHandler.java
@@ -1,8 +1,9 @@
 package webserver.CustomHandler;
 
 import java.io.IOException;
+import java.util.Map;
 
 public interface CustomHandler {
 
-    byte[] process() throws IOException;
+    byte[] process(Map<String, String> paramMap) throws IOException;
 }
--- src/main/java/webserver/CustomHandler/IndexHandler.java
+++ src/main/java/webserver/CustomHandler/IndexHandler.java
@@ -6,13 +6,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 
 public class IndexHandler implements CustomHandler{
 
     private final File file = new File("webapp/index.html");
 
     @Override
-    public byte[] process() throws IOException {
+    public byte[] process(Map<String, String> paramMap) throws IOException {
         BufferedReader fr = new BufferedReader(new FileReader(file));
         String fileData = IOUtils.readData(fr, (int) file.length());
         return fileData.getBytes();
