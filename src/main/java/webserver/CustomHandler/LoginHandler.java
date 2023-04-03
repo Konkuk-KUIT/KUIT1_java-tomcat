efbc6a000fc64d145cf36a624e51b213b9b75307
hhj <gudwn357@gmail.com>
2023-04-03 15:48:44 +0900

    feat (LoginHandler) : 응답 헤더 설정 추가

--- src/main/java/webserver/CustomHandler/LoginHandler.java
+++ src/main/java/webserver/CustomHandler/LoginHandler.java
@@ -2,6 +2,8 @@ package webserver.CustomHandler;
 
 import db.MemoryUserRepository;
 import model.User;
+import webserver.HttpRequest;
+import webserver.HttpResponse;
 
 import java.io.IOException;
 import java.util.Map;
@@ -15,19 +17,28 @@ public class LoginHandler implements CustomHandler{
     }
 
     @Override
-    public byte[] process(Map<String, String> paramMap) throws IOException {
+    public byte[] process(HttpRequest request, HttpResponse response) throws IOException {
         try {
+            Map<String, String> paramMap = request.getParamMap();
             User findById = repository.findUserById(paramMap.get("userId"));
-            return passwordCheck(paramMap, findById);
+            return passwordCheck(paramMap, findById, response);
         } catch (NullPointerException e) {
+            setStatusCodeAndLocation("/user/login_failed.html", response);
             return "/user/login_failed.html".getBytes();
         }
     }
 
-    private static byte[] passwordCheck(Map<String, String> paramMap, User findById) {
+    private static byte[] passwordCheck(Map<String, String> paramMap, User findById, HttpResponse response) {
         if (!findById.getPassword().equals(paramMap.get("password"))) {
             return "/user/login_failed.html".getBytes();
         }
+        setStatusCodeAndLocation("/index.html", response);
+        response.setHeader("Set-Cookie", "logined=true");
         return "/index.html".getBytes();
+    }
+
+    private static void setStatusCodeAndLocation(String location, HttpResponse response) {
+        response.setStatusCode("302");
+        response.setHeader("location", location);
     }
 }
