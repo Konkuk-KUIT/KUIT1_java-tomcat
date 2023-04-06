0a9d3a7483b1bd02418654000c6125d1bfd24b27
versatile0010 <siam0294@naver.com>
2023-04-06 23:24:04 +0900

    feat(HttpMethod): HTTP 의 method(POST, GET) 를 enum 으로 관리

--- src/main/java/webserver/RequestLine.java
+++ src/main/java/webserver/RequestLine.java
@@ -1,5 +1,7 @@
 package webserver;
 
+import webserver.http.HttpMethod;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -8,14 +10,18 @@ import static http.util.HttpRequestUtils.parseQueryParameter;
 
 public class RequestLine {
     private static final Logger logger = Logger.getLogger(RequestLine.class.getName());
-    private String method;
+    private HttpMethod httpMethod;
     private String path;
     private Map<String, String> params = new HashMap<>();
 
+    public HttpMethod getMethod() {
+        return httpMethod;
+    }
+
     public RequestLine(String requestLine) {
         String[] tokens = requestLine.split(" ");
-        method = tokens[0];
-        if (method.equals("POST")) {
+        httpMethod = HttpMethod.valueOf(tokens[0]);
+        if (httpMethod == HttpMethod.POST) {
             path = tokens[1];
             return;
         }
@@ -26,10 +32,6 @@ public class RequestLine {
             path = tokens[1].substring(0, index);
             params = parseQueryParameter(tokens[1].substring(index + 1));
         }
-    }
-
-    public String getMethod() {
-        return method;
     }
 
     public String getPath() {
