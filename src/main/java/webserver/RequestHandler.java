74ef5e14908bf1ab8a7c5e313fc25038512bcb7d
kang yeon joo <rkdduswn0409@gmail.com>
2023-04-06 15:57:35 +0900

    feat: 80포트 진입 시 index.html 파일 반환 기능(1단계)와 회원가입 관련기능(2단계) 기능 추가!
    RequestHandler 부분에서 요청 메세지에 따른 응답 메세지 처리를 해주었다.
    첫번째 if 문에서 localhost:80 처리를 해주었다. 두번쨰 else if문에서는
    /user/sign_up 으로 들어왔을 때 처리를 , 마지막 else 문에서는 그 외
    요청(사실 아직 2단계라 큰 의미가 없다)을 처리해주었다.
    
    Fixes

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,14 +1,27 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import db.Repository;
+import http.util.HttpRequestUtils;
+import model.User;
+
 import java.io.*;
 import java.net.Socket;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RequestHandler implements Runnable{
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
-
+    private static final Repository userRepository =MemoryUserRepository.getInstance();
     public RequestHandler(Socket connection) {
         this.connection = connection;
     }
@@ -16,17 +29,78 @@ public class RequestHandler implements Runnable{
     @Override
     public void run() {
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            DataOutputStream dos = new DataOutputStream(out);
+        try(BufferedReader is = new BufferedReader(new InputStreamReader(connection.getInputStream())); OutputStream os = connection.getOutputStream()){
+            String request = is.readLine();
+            log.log(Level.OFF, request);
+            String[] requestParts=request.split(" ");
 
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+            if(requestParts.length!=3){
+                throw new IllegalArgumentException("유효하지 않는 요청입니다.");
+            }
+
+            String method = requestParts[0]; //HTTP 요청 메서드
+            String path = requestParts[1]; //요청 경로
+            String version = requestParts[2]; //HTTP 프로토콜 버전
+            byte[] body;
+            log.log(Level.INFO,path);
+            DataOutputStream dos = new DataOutputStream(os);
+            if(path.equalsIgnoreCase("/")){
+                /******* 1단계 **********/
+                log.log(Level.OFF,"localhost:80");
+                body = Files.readAllBytes(Paths.get("C:/Users/rkddu/KUIT Server/java-tomcat/webapp/index.html"));
+                response200Header(dos, body.length);
+            }else if(path.startsWith("/user/signup")){
+                /******* 2단계 **********/
+                //회원 가입 요청 처리
+                //path.indexOf('?')을 이용하여 ? 문자가 있는 인덱스를 찾아낸 다음,
+                //substring 메소드를 사용하여 ? 문자 이후의 문자열을 추출한다.
+                String queryString = path.length()>1 ? path.substring(path.indexOf('?')+1) : "";
+                log.log(Level.INFO,queryString);
+                Map<String,String> paramMap = parseQueryString(queryString);
+
+                User user=new User(paramMap.get("userId"),paramMap.get("password"),paramMap.get("name"),paramMap.get("email"));
+                userRepository.addUser(user);
+
+                //페이지 리다이렉팅!
+                String redirectUrl = "http://localhost:80";
+                response302Header(dos,redirectUrl);
+                body=null;
+            }
+            else {
+                /******* 전체적으로 요청에 해당하는 파일 반환하는 부분 **********/
+                Path filePath = Paths.get("C:/Users/rkddu/KUIT Server/java-tomcat/webapp" + path);
+                body = Files.readAllBytes(filePath);
+                response200Header(dos, body.length);
+            }
+            //byte[] body = "Hello World".getBytes();
+            //byte[] body = Files.readAllBytes(Paths.get("C:/Users/rkddu/KUIT Server/java-tomcat/webapp/index.html"));
             responseBody(dos, body);
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
         }
+    }
+
+    private Map<String,String> parseQueryString(String queryString){
+        Map<String,String> paramMap = new HashMap<>();
+        if(!queryString.isEmpty()){
+            String[] parts=queryString.split("&");
+            for(String part:parts) {
+                String[] keyValue = part.split("=");
+                String key=keyValue[0]; //key는 아이디, 비번, 이름, 이메일 주소 "타입 정보!"
+                log.log(Level.INFO,key);
+                String value = keyValue.length > 1 ? keyValue[1] : ""; //value는 해당 타입에 대한 실제 데이터 정보!
+                //{key: value, key: value, key: value, key: value} 형식!
+                try{
+                    value= URLDecoder.decode(value,"UTF-8");
+                    log.log(Level.INFO,value);
+                }catch (UnsupportedEncodingException e){
+                    e.printStackTrace();
+                }
+                paramMap.put(key,value);
+            }
+        }
+        return paramMap;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -37,6 +111,17 @@ public class RequestHandler implements Runnable{
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String redirectURI) {
+        try{
+            dos.writeBytes("HTTP/1.1 302 FOUND\r\n");
+            dos.writeBytes("Location: "+redirectURI+"\r\n");
+            dos.writeBytes("Content-Length: 0\r\n");
+            dos.writeBytes("\r\n");
+        }catch (IOException e){
+            log.log(Level.SEVERE,e.getMessage());
         }
     }
 
