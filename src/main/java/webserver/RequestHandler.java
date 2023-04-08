325cd55fe5822b95fe533158c0b1e31e834536c9
lyouxsun <leedrkr323@konkuk.ac.kr>
2023-04-08 11:49:12 +0900

    server 3주차 미션

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,31 +1,123 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import db.Repository;
+import http.util.IOUtils;
+import model.User;
+
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class RequestHandler implements Runnable{
+import static http.util.HttpRequestUtils.parseQueryParameter;
+
+public class RequestHandler implements Runnable {
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
 
-    public RequestHandler(Socket connection) {
+    //요구사항.1
+
+    //Path pathIndex = Paths.get("/Users/lyouxsun/Desktop/java-tomcat/webapp/index.html");
+    //FileInputStream input = new FileInputStream("/Users/lyouxsun/Desktop/java-tomcat/webapp/index.html");
+
+    private static final String ROOT_URL =  "./webapp";
+    private static final String HOME_URL = "/index.html";
+    //private static final String LOGIN_FAILED_URL = "/user/login_failed.html";
+    //private static final String LOGIN_URL = "user/login.html";
+    //private static final String LIST_URL = "/user/list.html";
+
+
+    private final Repository repository;
+    private final Path homePath = Paths.get(ROOT_URL + HOME_URL);
+    public RequestHandler(Socket connection){ // throws FileNotFoundException {
         this.connection = connection;
+        repository = MemoryUserRepository.getInstance();
     }
+
 
     @Override
     public void run() {
+        //TODO의 실질적인 처리
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
+            byte[] body = new byte[0];      //왜 0?
+
+            //Header분석
+            //-> 입력받은 string에서 HTTP메서드(ex.get)와 url을 구분지어 저장하기 위해서 사용!!!
+            // startLine은 http 요청 응답 메시지 중 시작줄을 의미한다!!
+            // - 요청 시작줄 → 메서드 + 주소 + 버전 <<< ex : GET [url] HTTP/1.1 >>>
+
+            String startLine = br.readLine();
+            String[] startLines = startLine.split(" ");     //띄어쓰기 구분해서 입력받아주는 메서드
+            String method = startLines[0];  // method에는 http메서드를 저장
+            String url = startLines[1];     // url에는 주소를 저장
+
+            int requestContentLength = 0;
+            String cookie = "";
+
+            //요구사항3
+            while (true) {
+                final String line = br.readLine();
+                if (line.equals("")) {
+                    break;
+                }
+                // header info
+                if (line.startsWith("Content-Length")) {
+                    requestContentLength = Integer.parseInt(line.split(": ")[1]);
+                }
+
+                if(line.startsWith("Cookie")){
+                    body = Files.readAllBytes(Paths.get(ROOT_URL + url));
+                }
+            }
+            /**
+             * request body안의 정보를 읽어오기 위해서는 Header의 Content-Length 값이 필요하고,
+             * BufferedReader의 offset을 body 값 바로 앞에 위치시켜야한다.
+             * 이걸 구현한게 위 코드이다.
+             */
+
+
+            //요구사항1
+            //body = Files.readAllBytes(pathIndex);
+
+            //form.html까지 들어가는거 성공!!!
+            if(method.equals("GET") && url.endsWith(".html")){
+                body = Files.readAllBytes(Paths.get(ROOT_URL + url));
+            }
+            if(url.equals("/")){
+                body = Files.readAllBytes(homePath);
+            }
+
+            //요구사항2
+            /**
+             * 2. 정보를 입력하면 쿼리스트링으로부터 MomoryUserRepository에 정보를 받아와서
+             *    다시 index.html 화면을 띄운다
+             */
+
+            if(url.equals("/user/signup")){
+                String queryString = IOUtils.readData(br, requestContentLength);
+                Map<String, String> queryParameter = parseQueryParameter(queryString);      //queryString의 정보를 parsing하는 메서드
+                User user = new User(queryParameter.get("userId"), queryParameter.get("password"), queryParameter.get("name"), queryParameter.get("email"));
+                //새로운 user객체 생성
+                repository.addUser(user);
+                //user객체의 정보를 MemoryUserRepository에 저장
+                response302Header(dos, HOME_URL);
+                //dos
+                return;
+            }
+
             response200Header(dos, body.length);
             responseBody(dos, body);
-
         } catch (IOException e) {
-            log.log(Level.SEVERE,e.getMessage());
+            log.log(Level.SEVERE, e.getMessage());
         }
     }
 
@@ -49,4 +141,29 @@ public class RequestHandler implements Runnable{
         }
     }
 
+    private void response302Header(DataOutputStream dos, String path){
+        try{
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Location: " + path + "\r\n");
+            dos.writeBytes("Set-Cookie: logined=true" + "\r\n");
+            dos.writeBytes("\r\n");
+        }catch(IOException e){
+            log.log(Level.SEVERE, e.getMessage());
+        }
+
+        /*
+        private void response302HeaderWithCookie(DataOutputStream dos, String path){
+            try{
+                dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+                dos.writeBytes("Location: " + path + "\r\n");
+                dos.writeBytes("Set-Cookie: logined=true" + "\r\n");
+
+                dos.writeBytes("\r\n");
+            }catch(IOException e){
+                log.log(Level.SEVERE, e.getMessage());
+            }
+        }
+         */
+
+    }
 }
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,6 +1,6 @@
 package webserver;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -14,20 +14,29 @@ public class WebServer {
 
     public static void main(String[] args) throws IOException {
         int port = DEFAULT_PORT;
+        //생성될 스레드의 개수를 제안함 -> 너무 많이 만들면 한번에 요청을 처리하다 서버가 다운될 수도 있으니까
         ExecutorService service = Executors.newFixedThreadPool(DEFAULT_THREAD_NUM);
+
+        /*
+        File file = new File("/Users/lyouxsun/Desktop/java-tomcat/webapp/index.html");
+        FileInputStream output = new FileInputStream(file);
+        */
 
         if (args.length != 0) {
             port = Integer.parseInt(args[0]);
         }
+        //try-with-resource
+        //-> try가 끝나자마자 (try에서) 사용한 resource들을 자동으로 반납한다
 
         // TCP 환영 소켓
         try (ServerSocket welcomeSocket = new ServerSocket(port)){
-
-            // 연결 소켓
+            // 연결 소켓f
             Socket connection;
-            while ((connection = welcomeSocket.accept()) != null) {
+            while ((connection = welcomeSocket.accept()) != null) {     //  while문을 통해 지속적인 통신이 가능하도록 만들기
+                //input/outputStream과 관련된 기능은 RequestHandler의 run 메서드에서 구현
                 // 스레드에 작업 전달
                 service.submit(new RequestHandler(connection));
+
             }
         }
 
