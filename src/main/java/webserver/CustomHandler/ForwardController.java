ae3cc99d0d759fb012b6f887d5b9649789c71119
hhj <gudwn357@gmail.com>
2024-01-09 00:15:13 +0900

    refactor (ForwardController)
    FileIO 작업 HttpResponse로 이동

--- src/main/java/webserver/CustomHandler/ForwardController.java
+++ src/main/java/webserver/CustomHandler/ForwardController.java
@@ -1,24 +1,15 @@
 package webserver.CustomHandler;
 
-import http.util.IOUtils;
 import webserver.HttpRequest;
 import webserver.HttpResponse;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-
-import static webserver.constant.Http.*;
 
 public class ForwardController implements Controller {
 
-    private final File file = new File(WEBAPP.getValue() + INDEX.getValue());
 
     @Override
-    public byte[] process(HttpRequest request, HttpResponse response) throws IOException {
-        BufferedReader fr = new BufferedReader(new FileReader(file));
-        String fileData = IOUtils.readData(fr, (int) file.length());
-        return fileData.getBytes();
+    public void process(HttpRequest request, HttpResponse response) throws IOException {
+        response.forward(request.getRequestUri());
     }
 }
