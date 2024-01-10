112cf8bee9f43742e446a855da01a334dba15eb0
hhj <gudwn357@gmail.com>
2024-01-10 22:25:54 +0900

    refactor (UserListHandler)

--- src/main/java/webserver/CustomHandler/UserListHandler.java
+++ src/main/java/webserver/CustomHandler/UserListHandler.java
@@ -1,40 +1,22 @@
 package webserver.CustomHandler;
 
-import http.util.IOUtils;
 import webserver.HttpRequest;
 import webserver.HttpResponse;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
 
 import static webserver.constant.Http.*;
 
 public class UserListHandler implements Controller {
 
-    private final File file = new File(WEBAPP.getValue() + LIST.getValue());
-
     @Override
-    public byte[] process(HttpRequest request, HttpResponse response) throws IOException {
-        System.out.println("진입");
-        Map<String, String> headerMap = request.getHeaderMap();
-        String cookieValue = headerMap.get(COOKIE.getValue());
-        String[] split = cookieValue.split(";");
-        System.out.println("split[0] = " + split[0]);
-        if (split[0].equals(LOGINED_TRUE.getValue())) {
-            BufferedReader fr = new BufferedReader(new FileReader(file));
-            String fileData = IOUtils.readData(fr, (int) file.length());
-            return fileData.getBytes();
+    public void process(HttpRequest request, HttpResponse response) throws IOException {
+        String cookieValue = request.getHeader(COOKIE.getValue());
+        if (cookieValue == null || !cookieValue.equals(LOGINED_TRUE.getValue())) {
+            response.redirect(LOGIN_FORM.getValue());
+            return;
         }
-
-        setStatusCodeAndLocation(response);
-        return INDEX.getValue().getBytes();
+        response.forward(request.getRequestUri());
     }
 
-    private static void setStatusCodeAndLocation(HttpResponse response) {
-        response.setStatusCode(FOUND.getValue());
-        response.setHeader(LOCATION.getValue(), INDEX.getValue());
-    }
 }
